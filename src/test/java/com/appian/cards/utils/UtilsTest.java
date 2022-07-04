b38d89fdbaf84e074d211fe6af69b481d2a5b420
Alexander Perez Tase <alextase89@gmail.com>
2022-07-04 10:30:59 +0200

    commit missing test

--- src/test/java/com/appian/cards/utils/UtilsTest.java
+++ src/test/java/com/appian/cards/utils/UtilsTest.java
@@ -1,7 +1,14 @@
 package com.appian.cards.utils;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.appian.cards.model.Card;
+import com.appian.cards.model.FaceValueEnum;
+import com.appian.cards.model.SuiteTypeEnum;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class UtilsTest {
@@ -15,16 +22,32 @@ class UtilsTest {
     }
 
     @Test
-    void givenPositiveNumberOrZeroAsString_whenIsNumericAndNonNegative_thenTrue() {
+    void givenPositiveNumberOrZeroAsString_whenIsNumericAndNonNegative_thenReturnTrue() {
         assertTrue(Utils.isNumericAndNonNegative("0"));
         assertTrue(Utils.isNumericAndNonNegative("5"));
         assertTrue(Utils.isNumericAndNonNegative("8"));
     }
 
     @Test
-    void givenNonPositiveNumberOrZeroAsString_whenIsNumericAndNonNegative_thenFalse() {
+    void givenNonPositiveNumberOrZeroAsString_whenIsNumericAndNonNegative_thenReturnFalse() {
         assertFalse(Utils.isNumericAndNonNegative("-1"));
         assertFalse(Utils.isNumericAndNonNegative("-5"));
         assertFalse(Utils.isNumericAndNonNegative("One"));
+    }
+
+    @Test
+    void givenCardList_whenPrintableCardList_thenResultedStringHasSameSizeSeparatedByPipe() {
+        var cardList = List.of(
+            new Card(SuiteTypeEnum.CLUBS, FaceValueEnum.ACE),
+            new Card(SuiteTypeEnum.DIAMONDS, FaceValueEnum.FIVE),
+            new Card(SuiteTypeEnum.HEARTS, FaceValueEnum.FOUR),
+            new Card(SuiteTypeEnum.DIAMONDS, FaceValueEnum.NINE),
+            new Card(SuiteTypeEnum.SPADES, FaceValueEnum.SEVEN)
+        );
+
+        String printable = Utils.printableCardList(cardList);
+
+        assertNotNull(printable);
+        assertEquals(cardList.size(), printable.split("\\|").length);
     }
 }
